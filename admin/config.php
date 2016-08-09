448ca0df30742f20347b8aa991d880a675f0e91d
Lugano <ponomarev.link@gmail.com>
2016-08-09 19:09:58 +0300

    Start condition of shop

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart_new/admin/');
+define('HTTP_CATALOG', 'http://opencart_new/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart_new/admin/');
+define('HTTPS_CATALOG', 'http://opencart_new/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Apache/opencart_new/www/admin/');
+define('DIR_SYSTEM', 'C:/Apache/opencart_new/www/system/');
+define('DIR_LANGUAGE', 'C:/Apache/opencart_new/www/admin/language/');
+define('DIR_TEMPLATE', 'C:/Apache/opencart_new/www/admin/view/template/');
+define('DIR_CONFIG', 'C:/Apache/opencart_new/www/system/config/');
+define('DIR_IMAGE', 'C:/Apache/opencart_new/www/image/');
+define('DIR_CACHE', 'C:/Apache/opencart_new/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Apache/opencart_new/www/system/storage/download/');
+define('DIR_LOGS', 'C:/Apache/opencart_new/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Apache/opencart_new/www/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Apache/opencart_new/www/system/storage/upload/');
+define('DIR_CATALOG', 'C:/Apache/opencart_new/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456789');
+define('DB_DATABASE', 'new_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 're_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart_new/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart_new/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Apache/opencart_new/www/catalog/');
+define('DIR_SYSTEM', 'C:/Apache/opencart_new/www/system/');
+define('DIR_LANGUAGE', 'C:/Apache/opencart_new/www/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Apache/opencart_new/www/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Apache/opencart_new/www/system/config/');
+define('DIR_IMAGE', 'C:/Apache/opencart_new/www/image/');
+define('DIR_CACHE', 'C:/Apache/opencart_new/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Apache/opencart_new/www/system/storage/download/');
+define('DIR_LOGS', 'C:/Apache/opencart_new/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Apache/opencart_new/www/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Apache/opencart_new/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456789');
+define('DB_DATABASE', 'new_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 're_');
