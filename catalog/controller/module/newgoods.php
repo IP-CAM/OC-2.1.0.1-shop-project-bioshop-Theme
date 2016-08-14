91828e78f05abb4a24eccb20ffbf14889a4ded9a
Merecoder <ponomarev.link@gmail.com>
2016-08-14 19:17:06 +0300

    Update newgoods.php

--- catalog/controller/module/newgoods.php
+++ catalog/controller/module/newgoods.php
@@ -18,7 +18,7 @@ class ControllerModuleNewgoods extends Controller {
 		
 		$this->load->model('tool/image');
                 
-                $data['newgoods'] = 'newgoods' . $module++;
+                $data['newgoods'] = 'newgoods' . $module += 1;
 		
 		$data['products'] = array();		
 
